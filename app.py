8ba65f803f120cd11d5bf285e1b471862b2ed994
Genevieve Aguigwo <80175084+genna-eng@users.noreply.github.com>
2022-06-03 14:44:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.header ('Header')
 categories = ['a', 'b', 'c']
 st.multiselect ("Pick a category", categories)
 
-st.button("Click me!")
-if st.checkbox('Select me!')
-    st.write('you selected the check box')
+st.sidebar.button("Click me!")
+if st.checkbox("Select me!")
+    st.write("you selected the check box")
