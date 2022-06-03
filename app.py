83b1acd823aacea2900f70c6776dc0bf5abcc4d5
Genevieve Aguigwo <80175084+genna-eng@users.noreply.github.com>
2022-06-03 14:23:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-st.write ('IT Works check its 222!')
+st.write ('IT Works!' \n
+         'its 222, you saw 555 and 111 as always')
 st.header ('Header')
 
 categories = ['a', 'b', 'c']
